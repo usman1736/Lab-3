0677ad9cb497b3ea18675068ce444ecc348e2e26
Usman Basharat <usmanbasharat1027@gmail.com>
2025-11-20 15:07:25 -0700

    Lab 4 Completed

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,19 @@
+import ToDoForm from "@/components/ToDoForm";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import ToDoList from "../components/ToDoList";
 
 const App = () => {
   const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
   return (
     <View>
       <ToDoList task={tasks} />
+      <ToDoForm addTask={addTask} />
     </View>
   );
 };
---
+++ components/ToDoForm.jsx
@@ -0,0 +1,63 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React, { useState } from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+
+function ToDoForm({ addTask }) {
+  const [taskText, setTaskText] = useState("");
+
+  return (
+    <View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Add a new task..."
+          onChangeText={(text) => setTaskText(text)}
+          value={taskText}
+        />
+        <Button
+          title="Add"
+          onPress={() => {
+            addTask(taskText);
+            setTaskText("");
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  completed: {
+    backgroundColor: "#e0e0e0",
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default ToDoForm;
